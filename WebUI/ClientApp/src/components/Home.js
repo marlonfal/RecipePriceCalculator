8352559aafa2e5ce1e367aeba7a68f1e8534003c
Marlon Adarme <marlonfal@hotmail.com>
2021-12-24 16:34:29 -0500

    ui change

--- WebUI/ClientApp/src/components/Home.js
+++ WebUI/ClientApp/src/components/Home.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from 'react';
-import { Card, Button } from 'bootstrap';
 
 const Home = () => {
 
-
+    const [loading, setLoading] = useState(true);
     const [recipes, setRecipes] = useState([]);
 
     useEffect(() => {
         fetch('api/RecipeCost')
             .then(result => result.json())
-            .then(result => setRecipes(result))
+            .then(result => {
+                setLoading(false);
+                setRecipes(result)
+            })
             .catch(error => {
+                setLoading(false);
                 console.log(error);
             })
     }, [])
@@ -18,30 +21,35 @@ const Home = () => {
     return (
         <div className="row">
             {
-                recipes.map((recipe, i) => {
-                    return (
-                        <div key={recipe.recipe.recipeId} className="col-md-4 col-sm-12" style={{padding: "1rem"}}>
-                            <div className="card" style={{ width: "100%", height: "100%" }}>
-                                <h5 className="card-header">{recipe.recipe.name}</h5>
-                                <div className="card-body">
-                                    <p className="card-text">Ingredients</p>
-                                    <ul>
-                                        {
-                                            recipe.recipe.recipeProducts.map((product, j) => {
-                                                return <li key={recipe.recipe.recipeId + " " + product.productId}>{product.product.name}</li>;
-                                            })
-                                        }
+                loading ?
+                    <div className="spinner-border text-warning" role="status">
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                    :
+                    recipes.map((recipe, i) => {
+                        return (
+                            <div key={recipe.recipe.recipeId} className="col-md-4 col-sm-12" style={{ padding: "1rem" }}>
+                                <div className="card" style={{ width: "100%", height: "100%" }}>
+                                    <h5 className="card-header">{recipe.recipe.name}</h5>
+                                    <div className="card-body">
+                                        <p className="card-text">Ingredients</p>
+                                        <ul>
+                                            {
+                                                recipe.recipe.recipeProducts.map((product, j) => {
+                                                    return <li key={recipe.recipe.recipeId + " " + product.productId}>{product.product.name}</li>;
+                                                })
+                                            }
+                                        </ul>
+                                    </div>
+                                    <ul className="list-group list-group-flush">
+                                        <li className="list-group-item">Tax: {recipe.saleTaxFormatted}</li>
+                                        <li className="list-group-item">Discount: {recipe.wellnessDiscountFormatted}</li>
+                                        <li className="list-group-item">Total: {recipe.totalFormatted}</li>
                                     </ul>
                                 </div>
-                                <ul className="list-group list-group-flush">
-                                    <li className="list-group-item">Tax: {recipe.saleTaxFormatted}</li>
-                                    <li className="list-group-item">Discount: {recipe.wellnessDiscountFormatted}</li>
-                                    <li className="list-group-item">Total: {recipe.totalFormatted}</li>
-                                </ul>
                             </div>
-                        </div>
-                    )
-                })
+                        )
+                    })
             }
         </div>
     );
